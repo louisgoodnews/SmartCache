227f95f6d3cbf729d41720fd9eb8bb84f42ff79a
Louis Goodnews <louisgoodnews95@gmail.com>
2025-08-09 11:05:09 +0200

    Changed a few things.

---
+++ src/SmartCacheing/__init__.py
@@ -0,0 +1,15 @@
+"""
+Author: Louis Goodnews
+Date: 2025-08-09
+"""
+
+from typing import Final, List
+
+from .core import Cache, CacheItem, CacheService
+
+
+__all__: Final[List[str]] = [
+    "CacheItem",
+    "Cache",
+    "CacheService",
+]
